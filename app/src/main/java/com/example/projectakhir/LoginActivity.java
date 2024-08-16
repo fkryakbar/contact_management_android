88cfc62728be54901078c79d68f86022975208fa
fkryakbar <fikriafa289@gmail.com>
2024-08-16 15:19:38 +0800

    add login

---
+++ app/src/main/java/com/example/projectakhir/LoginActivity.java
@@ -0,0 +1,57 @@
+package com.example.projectakhir;
+
+import android.content.Intent;
+import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+import android.widget.EditText;
+import android.widget.Toast;
+
+import androidx.activity.EdgeToEdge;
+import androidx.appcompat.app.AppCompatActivity;
+import androidx.core.graphics.Insets;
+import androidx.core.view.ViewCompat;
+import androidx.core.view.WindowInsetsCompat;
+
+public class LoginActivity extends AppCompatActivity {
+
+    private EditText editTextUsername;
+    private EditText editTextPassword;
+    private Button buttonLogin;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        EdgeToEdge.enable(this);
+        setContentView(R.layout.activity_login);
+
+
+        editTextUsername = findViewById(R.id.edit_text_username);
+        editTextPassword = findViewById(R.id.edit_text_password);
+        buttonLogin = findViewById(R.id.button_login);
+
+        buttonLogin.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                String username = editTextUsername.getText().toString().trim();
+                String password = editTextPassword.getText().toString().trim();
+
+                if (username.isEmpty() || password.isEmpty()) {
+                    Toast.makeText(LoginActivity.this, "Please enter both username and password", Toast.LENGTH_SHORT).show();
+                } else {
+                    if(username.equals("fkryakbar")  && password.equals("123")){
+                        Toast.makeText(LoginActivity.this, "Login Successful", Toast.LENGTH_SHORT).show();
+                        Intent intent = new Intent(LoginActivity.this, MainActivity.class);
+                        startActivity(intent);
+
+                        finish();
+                    }else{
+                        Toast.makeText(LoginActivity.this, "Invalid Account", Toast.LENGTH_SHORT).show();
+                    }
+
+                }
+            }
+        });
+
+    }
+}
